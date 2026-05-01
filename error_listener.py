25c04ccb30b2a3d7840931c7dcbb1afc0cee2628
EdsonERGC <edsonrafael2003@gmail.com>
2026-04-30 22:58:33 -0600

    Implementación inicial LLVM IR Generator

--- error_listener.py
+++ error_listener.py
@@ -1,8 +1,13 @@
 from antlr4.error.ErrorListener import ErrorListener
 
 class MyErrorListener(ErrorListener):
+
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        if offendingSymbol.type == -1:
-            print(f"[ERROR LÉXICO] Línea {line}:{column} símbolo inválido")
+
+        # ERROR LÉXICO
+        if offendingSymbol is None:
+            print(f"[ERROR LÉXICO] Línea {line}:{column} -> símbolo no válido")
+
+        # ERROR SINTÁCTICO
         else:
             print(f"[ERROR SINTÁCTICO] Línea {line}:{column} -> {msg}")
