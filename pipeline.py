e6fc8e6444692fff30915f34e23a7fd69bbc4b93
EdsonERGC <160798709+EdsonERGC@users.noreply.github.com>
2026-06-05 13:19:34 -0600

    Refactor pipeline.py for LLVM IR generation

--- pipeline.py
+++ pipeline.py
@@ -90,29 +90,20 @@ def main():
 
     print("Archivo TAC generado en output/programa.tac")
 
- # =========================
-# 7.1. GENERAR LLVM IR
-# =========================
-print("\nGenerando código LLVM IR...")
+# //////////////////////////////////////////////////////////
+# LLVM IR V4
+# INTEGRANTE 4
+# //////////////////////////////////////////////////////////
+
+print("\n===== FASE 4: GENERACIÓN LLVM IR =====")
 
 ir_gen = IRGenerator()
-ir_gen.create_main()
 
-#  EJEMPLO BÁSICO (luego lo conectas al AST)
-# Aquí solo probamos que funcione
-ir_gen.declare_variable("x", 10)
+ir_gen.generate_from_tac(codigo_tac)
 
-x = ir_gen.load_variable("x")
-res = ir_gen.add(x, ir.Constant(ir.IntType(32), 5))
-
-ir_gen.finish()
-
-# Guardar archivo
-with open("output/programa.ll", "w") as f:
-    f.write(str(ir_gen.module))
+ir_gen.save("output/programa.ll")
 
 print("Archivo LLVM IR generado en output/programa.ll")
-
     # =========================
     # 8. INTERPRETAR
     # =========================
